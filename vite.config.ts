0b8284bbaa3b3b61c03d624cacfd8a0d7c267f10
steven hsu <steven@tx-StevenHsu.local>
2026-01-31 02:25:02 +0800

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/OT-Risk-Measurement-Simulation/',
   plugins: [react()],
   resolve: {
     alias: [
